2c16261e8c9f797934dc291609d2c0070add7d0d
Santiago Prieto <santiagoprietoacero@gmail.com>
2020-01-30 16:50:15 -0500

    add products

---
+++ src/.env.development.js
@@ -0,0 +1,5 @@
+STRIPE_SK = 'sk_test_WZjpudkrAZ345I2BKLUMwCi600xNcOa2I9'
+STRIPE_PK = 'pk_test_wBeLjvwyL5kBo4dG96c4497t00Zh1yeHk9'
+SUCCES_REDIREC = 'http://localhost:8000/gracias'
+CANCEL_REDIRECT = 'http://localhost:8000/gracias'
+
